addce63b2040ef055a15a226cd8088a3f3134fef
@Mohsens22 <mohsens2@outlook.com>
2017-09-15 15:24:12 +0430

    Cleaned project and added a new project

--- SepidRahUWP/Sepidrah.UWP/Properties/AssemblyInfo.cs
+++ SepidRahUWP/Sepidrah.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SepidRahUWP")]
+[assembly: AssemblyTitle("Sepidrah.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SepidRahUWP")]
+[assembly: AssemblyProduct("Sepidrah.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
